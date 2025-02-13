f62a6479701239dabd6c6e741e2e99151d9701e8
Plauuss <deluxerelax4@gmail.com>
2025-02-13 11:30:23 +0800

    Divina
    Feb 12-13 1st Activity at Lab 2

---
+++ IT13B_Divina_Activities/src/Prelim/Lab_act1_Arithmetics.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prelim;
+
+import java.util.Scanner;
+
+
+public class Lab_act1_Arithmetics { 
+        public static void main(String[] args){
+        
+        int num1 = 10;
+        int num2 = 4;
+        int num3 = 6;
+        System.out.println(num1 + "*" + num2 + "+" + num3 + "=" + (num1 * num2 + num3));
+        System.out.println(num1 + "-" + num2 + "%" + num3 + "=" + ((num1 - num2) % num3));
+        System.out.println(num1 + "+" + num2 + "+" + num3 + "=" + (num1 + num2 + num3));
+        System.out.println(num1 + "*" + num3 + "-" + num2 + "*" + num2 + "=" + (num1 * num3 - num2 * num2));                
+    
+                
+        
+    }
+            
+}
