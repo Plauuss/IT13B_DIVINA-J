c26e383c8b6bd085068cd231f37a1fe7fda55b40
Jibs005 <deluxerelax4@gmail.com>
2025-02-19 09:53:37 +0800

    Added Act 1
    Updated

---
+++ IT13B_Divina_Activities/IT13B_Divina_Activities/src/it13b_divina_activities/IT13B_Divina_Activities.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package it13b_divina_activities;
+
+/**
+ *
+ * @author User
+ */
+public class IT13B_Divina_Activities {
+
+   public static void main(String[] args){
+        
+        int num1 = 10;
+        int num2 = 4;
+        int num3 = 6;
+        System.out.println(num1 + "*" + num2 + "+" + num3 + "=" + (num1 * num2 + num3));
+        System.out.println(num1 + "-" + num2 + "%" + num3 + "=" + ((num1 - num2) % num3));
+        System.out.println(num1 + "+" + num2 + "+" + num3 + "/" + 3 + "=" + (num1 + num2 + num3) / 3);
+        System.out.println(num1 + "*" + num3 + "-" + num2 + "*" + num2 + "=" + (num1 * num3 - num2 * num2)); 
+    
+    }
+    
+}
